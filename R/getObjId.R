7382c255114fa1bd86c31bb4e4f7f36600b19eae
Job Spijker <job.spijker@rivm.nl>
2019-10-25 14:05:23 +0200

    fixed id vs did issue

--- R/getObjId.R
+++ R/getObjId.R
@@ -7,10 +7,10 @@ getObjId <- function(name) {
 
 	s <- getOption("pgobject.schema")
 	res<-FALSE
-	d<-sql(paste("select id from ",s,".robjects where name='",
+	d<-sql(paste("select did from ",s,".robjects where name='",
 				 name,"'",sep=''))
 	if (!is.na(d) && nrow(d)==1) {
-		res <- d$id[1]
+		res <- d$did[1]
 	} else {
 		res <- NA
 	}
